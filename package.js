e5188b5dac8e70aa7c146e36b63c2b5cad7c8c15
yuzzhadedev <mutustorereal@gmail.com>
2026-04-17 13:04:29 +0700

    Initialize package.json with project details

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "yuzz-hade",
+  "version": "1.0.0",
+  "description": "Yuzz Hade SUPER HD",
+  "main": "index.html",
+  "dependencies": {
+    "axios": "^1.6.0",
+    "moment-timezone": "^0.5.43"
+  },
+  "engines": {
+    "node": ">=18.x"
+  }
+}
